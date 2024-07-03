a7b99d76900833b005c2d825fad3b8d4153cafe1
ananadnair10 <anandnair1965@gmail.com>
2024-07-03 16:39:30 +0530

    changes in footer and hero

--- avps/src/components/Footer.jsx
+++ avps/src/components/Footer.jsx
@@ -6,17 +6,20 @@ function Footer() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col sm:flex-row justify-between items-center">
                     <div className="text-center sm:text-left mb-4 sm:mb-0">
-                        <p>&copy; {new Date().getFullYear()} ACPS. All rights reserved.</p>
+                        <p>&copy; {new Date().getFullYear()} AVPS. All rights reserved.</p>
                     </div>
                     <div className="flex space-x-4">
-                        <a href="#" className="hover:underline">
+                        {/* <a href="#" className="hover:underline">
                             Privacy Policy
-                        </a>
-                        <a href="#" className="hover:underline">
+                        </a> */}
+                        <a href="https://github.com/anandnair10/AVPS" className="hover:underline">
                             Terms of Service
                         </a>
-                        <a href="#" className="hover:underline">
-                            Contact Us
+                        <a href="#" className="">
+                            <p>Ph: +91 94951 21351</p>
+                        </a>
+                        <a href="#" className="">
+                             <p>e-mail: kte21cs017@cet.ac.in</p>
                         </a>
                     </div>
                 </div>
